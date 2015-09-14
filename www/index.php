78932e5eaa79b7ed51ffb46cb46bad774a8705f8
Marek Cepcek <marek.cepcek@gmail.com>
2015-09-14 21:28:24 +0200

    pista je pan

--- www/index.php
+++ www/index.php
@@ -2,7 +2,7 @@
 
 // Uncomment this line if you must temporarily take down your site for maintenance.
 // require __DIR__ . '/.maintenance.php';
-
+echo 'som pan :D';
 $container = require __DIR__ . '/../app/bootstrap.php';
 
 $container->getByType('Nette\Application\Application')->run();
